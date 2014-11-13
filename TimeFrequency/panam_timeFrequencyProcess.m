65d8d4953f904364bd982f01bdb4514640b7d148
jeledouget <jeaneudes.ledouget@gmail.com>
2014-11-13 14:55:55 +0100

    Time Frequency routines 1

--- TimeFrequency/panam_timeFrequencyProcess.m
+++ TimeFrequency/panam_timeFrequencyProcess.m
@@ -3,6 +3,44 @@ function outputStruct = panam_timeFrequencyProcess( inputStructs, inputEvents, p
 %Panam TimeFrequency structures
 
 
+%% load inputs
+% check if the input structures are file adresses or actual matlab structures
+% in case of file adresses, load the structures
+
+
+%% default parameters
+% define the default parameters
+
+
+%% check/affect parameters
+
+
+%% average contacts
+% define the contacts selected for each input structure
+% then average over the contacts
+
+
+%% trials filtering
+% select trials which correpond to the specified filter
+
+
+%% subject averaging
+% in case of multi-subjcet inputs, average over subjects if option is selected
+
+
+%% baseline correction
+% apply baseline correction on the trials/subjects :
+% decibel - zscore - ratio of change from baseline - (average t-maps ?)
+
+
+%% events handling
+% compute events averages
+
+
+%% output affectation
+
+
+%% visualization
 
 
 end
