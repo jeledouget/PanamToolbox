692363bda217d52791f917ea7dd0a13c02729b8f
jeledouget <jeaneudes.ledouget@gmail.com>
2015-09-11 01:53:13 +0200

    Correction (bl)

--- DataStructure/@TimeSignal/correction.m
+++ DataStructure/@TimeSignal/correction.m
@@ -1,6 +1,7 @@
 % Method for class 'TimeSignal' and subclasses
-%  adjustTime : for muti-elements TimeSignal, make it that all elements
-%  have the same Time axis. Useful before averaging for example
+%  correction : apply a correction to the data. For example zscore, db...
+% Baseline can be specified through absolute Time or relative to an event
+% or 2
 % INPUTS
 % OUTPUT
 
@@ -11,30 +12,102 @@ function newSignal = correction(self, corrType, varargin)
 % copy of the object
 newSignal = self;
 
-% args & options
+% options
 if ~isempty(varargin)
-    if ischar(varargin{1}) % kvPairs
-        varargin = panam_args2struct(varargin);
-    else % structure
-        varargin = varargin{1};
+    if ischar(varargin{1})
+        isEV = 1;
+        ev1 = varargin{1};
+        if ischar(varargin{2})
+            ev2 = varargin{2};
+            delay = varargin{3};
+        else
+            ev2 = varargin{1};
+            delay = varargin{2};
+        end
+    else
+        isEV = 0;
+        time = varargin{1};
     end
 else
-    varargin = [];
+    isEV = 0;
+    time = 'all';
 end
 
-switch lower(corrType)
-    case 'zscore'
-        
-    case 'db'
-        
-    case {'relativechange', 'relchange'}
-        
+
+
+for i = 1:numel(newSignal)
+    
+    % baseline times
+    % correction is applied between the baseline start to the next baseline
+    % start (ex stimulus onset minus 3s to 3s before next stimulus onset)
+    if ~isEV
+       if isequal(time,'all')
+           samples{1} = [1 numel(self(i).Time)];
+       else
+           if times(1) < self(i).Time(1) || times(2) > self(i).Time(end)
+               error('input times to compute correction are not present in the Time vector');
+           end
+           if times(2) < times(1)
+               error('times for correction must be a 2-elements increasing vector');
+           end
+           samples{1} = [panam_closest(self(i).Time, time(1)) panam_closest(self(i).Time, time(2))];
+       end
+    else
+        events = self(i).Events.unifyEvents;
+        eventNames = {events.EventName};
+        ind1 = strcmp(eventNames, ev1);
+        ind2 = strcmp(eventNames, ev2);
+        times1 = events(ind1).Time + delay(1);
+        times2 = events(ind2).Time + delay(2);
+        if numel(times1) ~= numel(times2) || any(times2 - times1 < 0)
+            error('problem with the choice of events and/or delays');
+        end
+        for j = 1:numel(times1)
+            samples{j}(1) = panam_closest(self(i).Time, times1(j));
+            samples{j}(2) = panam_closest(self(i).Time, times2(j));
+        end
+    end
+    
+    % apply correction
+    data = self(i).Data;
+    sz = size(data);
+    data = reshape(data, sz(1),[]);
+    for j = 1:numel(samples)
+        % indices
+        if j==1
+            tmpSample(1) = 1;
+        else
+            tmpSample(1) = samples{j}(1);
+        end
+        if j==numel(samples)
+            tmpSample(2) = sz(1);
+        else
+            tmpSample(2) = samples{j}(2);
+        end
+        % compute
+        switch lower(corrType)
+            case 'zscore'
+                m = repmat(nanmean(data(samples{j}(1):samples{j}(2),:),1),numel(tmpSample(1):tmpSample(2)),1);
+                s = repmat(nanstd(data(samples{j}(1):samples{j}(2),:),0,1),numel(tmpSample(1):tmpSample(2)),1);
+                data(tmpSample(1):tmpSample(2),:) = (data(tmpSample(1):tmpSample(2),:) - m) ./ s;
+            case 'db'
+                m = repmat(nanmean(data(samples{j}(1):samples{j}(2),:),1),numel(tmpSample(1):tmpSample(2)),1);
+                data(tmpSample(1):tmpSample(2),:) = 10*log10(data(tmpSample(1):tmpSample(2),:) ./ m);                
+            case {'relativechange', 'relchange'}
+                m = repmat(nanmean(data(samples{j}(1):samples{j}(2),:),1),numel(tmpSample(1):tmpSample(2)),1);
+                data(tmpSample(1):tmpSample(2),:) = 100*(data(tmpSample(1):tmpSample(2),:) - m) ./ m;
+            case 'difference'
+                m = repmat(nanmean(data(samples{j}(1):samples{j}(2),:),1),numel(tmpSample(1):tmpSample(2)),1);
+                data(tmpSample(1):tmpSample(2),:) = data(tmpSample(1):tmpSample(2),:) - m;
+        end
+    end
+    newSignal(i).Data = reshape(data, sz);
 end
 
 % history
 for ii = 1:numel(newSignal)
     newSignal(ii).History{end+1,1} = datestr(clock);
     newSignal(ii).History{end,2} = ...
-        ['Correct the signal'];
+        ['Correct the signal with ' corrType];
 end
 end
